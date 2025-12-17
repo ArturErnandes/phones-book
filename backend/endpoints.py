9d64a35e8245b2a7b2f9a6a068103310da7724ad
Artur PC <al211205@yandex.ru>
2025-12-17 18:55:41 +0300

    Добавлены ручки для главной страницы и получения номеров

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,1 +1,19 @@
 #Файл с эндпоинтами
+from fastapi import APIRouter, HTTPException
+
+from pydantic import BaseModel
+
+router = APIRouter()
+
+
+
+@router.get("/info", tags=["Главная страница"], summary="Отображения текста на главной странице")
+def get_info():
+    return {"message": "Телефонный справочник"}
+
+
+@router.get("/subscribers", tags=["Абоненты"], summary="Получение спииска абонентов")
+def get_subscribers():
+    ...
+
+    return {"success": True}
