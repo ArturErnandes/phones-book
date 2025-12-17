a89229571fc1b572dd1f195ef28c2df20de1ae90
Artur PC <al211205@yandex.ru>
2025-12-17 18:55:22 +0300

    Будущий фукц

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,8 @@
 #Файл с функционалом взаимодействия с БД
+
+
+
+engine = create_async_engine(f'postgresql+asyncpg://postgres:{db_pass}@localhost:{db_port}/contacts_mai')
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
